df7e58f9534a86d05b8e9c11c3f59c8a96f2a6f7
Rohan Gupta <guptarohan369@gmail.com>
2021-07-11 21:47:26 +0530

    Add monitor, stale and build workflows Changes - Add snyk monitor, stale and gh-pages deploy workflows - Update labeler.yml

--- src/resources/constants.ts
+++ src/resources/constants.ts
@@ -1,5 +1,6 @@
 const BOOKS_API_URI_DEV: string = 'http://localhost:5000/api/v1/books';
-const BOOKS_API_URI_PROD: string = 'http://localhost:5000/api/v1/books';
+const BOOKS_API_URI_PROD: string =
+    'https://appunti-server.herokuapp.com/api/v1/books';
 
 export const BOOKS_API_URI: string =
     process.env.NODE_ENV === 'development'
