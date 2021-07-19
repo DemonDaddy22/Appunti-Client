3204e1d7fc96bfd38a7df81f9aa2d68e8098e356
Rohan Gupta <guptarohan369@gmail.com>
2021-07-19 23:59:57 +0530

    Make header responsive

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import Quill from '../../assets/icons/Quill';
 import classes from './styles.module.scss';
 
+// TODO- make header responsive
+// TODO- create navbar
 const AppHeader: React.FC<IAppHeader> = (props) => {
     const { label, showIcon, style } = props;
 
     return (
         <div className={classes.headerContainer}>
-            <Quill />
+            {showIcon && (
+                <div className={classes.iconWrapper}>
+                    <Quill style={{ height: 'inherit', width: 'inherit' }} />
+                </div>
+            )}
             <div className={classes.header} style={style}>
                 {label}
             </div>
@@ -16,3 +22,5 @@ const AppHeader: React.FC<IAppHeader> = (props) => {
 };
 
 export default AppHeader;
+
+AppHeader.defaultProps = { showIcon: true };
