28b9bbc4e4727f3b39cb231d678fd399e4ca1994
Rohan Gupta <guptarohan369@gmail.com>
2021-07-22 01:27:49 +0530

    Add scrolling event listener on navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,28 +1,48 @@
-import React, { useContext } from 'react';
+/* eslint-disable prettier/prettier */
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { ThemeContext } from '../../context/ThemeContext';
+import { NAVBAR_SCROLL_BREAKPOINT_HEIGHT } from '../../resources/constants';
 import AppHeader from '../AppHeader';
 import ThemeToggler from '../ThemeToggler';
 import classes from './styles.module.scss';
 
-// TODO- add event listener to update background color of navbar on scrolling
+// TODO- throttle page scrolling function calls
 const Navbar: React.FC<INavbar> = (props) => {
     const { style } = props;
     const { getThemedValue } = useContext(ThemeContext);
 
+    const [isPageScrolling, setIsPageScrolling] = useState<boolean>(false);
+
+    const handlePageScrolling = useCallback(() => {
+        setIsPageScrolling(
+            window.scrollY > NAVBAR_SCROLL_BREAKPOINT_HEIGHT ? true : false
+        );
+    }, [window.scrollY]);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handlePageScrolling);
+        return () => window.removeEventListener('scroll', handlePageScrolling);
+    }, []);
+
     return (
-        <div
+        <nav
             className={classes.navbar}
             style={{
                 ...style,
-                backgroundColor: getThemedValue(
-                    'rgba(255, 255, 255, 0.1)',
-                    'rgba(32, 38, 54, 0.25)'
-                ),
+                backgroundColor: isPageScrolling
+                    ? getThemedValue(
+                        'rgba(255, 255, 255, 0.1)',
+                        'rgba(32, 38, 54, 0.25)'
+                    )
+                    : 'transparent',
+                backdropFilter: isPageScrolling
+                    ? 'blur(10px) saturate(120%) contrast(60%) brightness(135%)'
+                    : 'none'
             }}
         >
             <AppHeader showIcon={false} label="Appunti" style={{ flex: 1 }} />
             <ThemeToggler />
-        </div>
+        </nav>
     );
 };
 
